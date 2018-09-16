7670db4d5e709f673d391c3b4e16e399662f1cd9
Mike Wu <mikejcwu@gmail.com>
2018-09-16 15:19:11 -0700

    <top, mid, bot border are done>

--- homework/assignment1.js
+++ homework/assignment1.js
@@ -6,10 +6,36 @@ function drawLine (numLine){
     let drawLine="";
     
     for(let i=0; i<numLine;i+=1){
-        drawLine = drawLine + ("_");
+        drawLine = drawLine + ("━");
     }
-
     return drawLine;
 }
+//--------------TOP BORDER---------------------
+function drawTopBorder(numTopBorder){
+    if(isNaN(numTopBorder)){
+        return "NOT A NUMBER!"
+    }
+    let drawTopBorder1="┏"+drawLine(numTopBorder)+"┓";
+    return drawTopBorder1;
+}
+//--------------MIDDLE BORDER---------------------
+function drawMiddleBorder(numMiddleBorder){
+    if(isNaN(numMiddleBorder)){
+        return "NOT A NUMBER!"
+    }
+    let drawMidBorder="┣"+drawLine(numMiddleBorder)+"┫";
+    return drawMidBorder;
+}
+//--------------BOTTOM BORDER---------------------
+function drawBottomBorder(numBottomBorder){
+    if(isNaN(numBottomBorder)){
+        return "NOT A NUMBER!"
+    }
+    let drawBotBorder="┗"+drawLine(numBottomBorder)+"┛";
+    return drawBotBorder;
+}
 
-console.log(drawLine("A"));
+
+console.log(drawTopBorder(10));
+console.log(drawMiddleBorder(10));
+console.log(drawBottomBorder(10));
