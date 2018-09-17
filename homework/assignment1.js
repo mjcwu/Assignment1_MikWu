ca44c19cc15fa10cbbb9169c94500f4f3a7b8e57
Mike Wu <mikejcwu@gmail.com>
2018-09-16 18:18:24 -0700

    <done first box script>

--- homework/assignment1.js
+++ homework/assignment1.js
@@ -1,7 +1,7 @@
 function drawLine (numLine){
-        if(isNaN(numLine)){
-        return "NOT A NUMBER!"
-    }
+    //     if(isNaN(numLine)){
+    //     return "NOT A NUMBER!"
+    // }
 
     let drawLine="";
     
@@ -12,30 +12,71 @@ function drawLine (numLine){
 }
 //--------------TOP BORDER---------------------
 function drawTopBorder(numTopBorder){
-    if(isNaN(numTopBorder)){
-        return "NOT A NUMBER!"
-    }
+    // if(isNaN(numTopBorder)){
+    //     return "NOT A NUMBER!"
+    // }
     let drawTopBorder1="┏"+drawLine(numTopBorder)+"┓";
     return drawTopBorder1;
 }
 //--------------MIDDLE BORDER---------------------
 function drawMiddleBorder(numMiddleBorder){
-    if(isNaN(numMiddleBorder)){
-        return "NOT A NUMBER!"
-    }
+    // if(isNaN(numMiddleBorder)){
+    //     return "NOT A NUMBER!"
+    // }
     let drawMidBorder="┣"+drawLine(numMiddleBorder)+"┫";
     return drawMidBorder;
 }
 //--------------BOTTOM BORDER---------------------
 function drawBottomBorder(numBottomBorder){
-    if(isNaN(numBottomBorder)){
-        return "NOT A NUMBER!"
-    }
+    // if(isNaN(numBottomBorder)){
+    //     return "NOT A NUMBER!"
+    // }
     let drawBotBorder="┗"+drawLine(numBottomBorder)+"┛";
     return drawBotBorder;
 }
 
-
-console.log(drawTopBorder(10));
-console.log(drawMiddleBorder(10));
-console.log(drawBottomBorder(10));
+//--------------DRAW BARS---------------------
+function drawBars(userString, maxStrNum){
+    // if(typeof userString!= "string"){
+    //     return "NOT A STRING!"
+    // }
+    let drawBarsAround="┃"+userString;
+    
+        if(userString.length===maxStrNum){
+            drawBarsAround=drawBarsAround+"┃";
+            return drawBarsAround;
+        } else {
+            for(let m=userString.length;m<maxStrNum;m+=1){
+            drawBarsAround = drawBarsAround + " ";
+            }
+        }
+    drawBarsAround = drawBarsAround + "┃";
+    return drawBarsAround;
+}
+//--------------Box It---------------------
+function boxIt(userArray){
+    let maxStr=0;
+    for(let k=0;k<userArray.length;k+=1){
+        for(let index of userArray){
+            if(userArray[k].length<index.length){
+                maxStr=index.length;
+            }
+        }
+    }
+    console.log("max="+maxStr)
+    let modifiedString = drawTopBorder(maxStr); 
+    if(userArray.length===1){
+        modifiedString = modifiedString + "\n"+drawBars(userArray[0]);
+    } else {
+        for(let j=0; j < userArray.length; j+=1){
+            if(j===(userArray.length-1)){
+                modifiedString = modifiedString + "\n"+drawBars(userArray[j],maxStr);
+            } else {
+                modifiedString = modifiedString + "\n"+drawBars(userArray[j],maxStr)+ "\n"+ drawMiddleBorder(maxStr);
+            } 
+        }
+    }
+    modifiedString = modifiedString+"\n"+drawBottomBorder(maxStr);
+    return modifiedString; 
+}
+console.log(boxIt(["I love you Catherine Wong","Iron man","Hulk","Black Widow","Drax the Destoryer"]));
